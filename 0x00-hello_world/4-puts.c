d3b2d8339a0d61e316d795af1a3a59321c67d366
Decypher1 <mumekwe2@gmail.com>
2022-03-10 06:03:41 -0800

    Hello printf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,18 +4,16 @@
 
  /*
  *
- *  main - Starting point
+ *  main - Entry point
  *
  *  Return: Always 0 (Success)
  *
  */
 
 int main(void)
-
 {
 
 		puts("\"Programming is like building a multilingual puzzle");
 
 			return (0);
-
 }
