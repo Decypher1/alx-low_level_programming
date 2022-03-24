9197847462d4027fc3c229726da96ce7aa12bab6
Decypher1 <mumekwe2@gmail.com>
2022-03-24 10:17:18 -0700

    concatenates a string

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,30 +1,46 @@
+#include <stdio.h>
 #include "main.h"
 
 /**
- * _strcat - concatenates two strings
- * @dest: string to append to
- * @src: string to add
- *
- * Return: a pointer to the resulting string
+ *_strlen - returns the length of a string
+ *@str:a string of length to be returned
+ *Return: returns the length of a string
+ */
+int _strlen(char *str)
+{
+	int length = 0;
+
+	while (*str)
+	{
+		str++;
+		length++;
+	}
+
+	return (length);
+
+}
+
+
+/**
+ *_strcat - concatinates two strings
+ *@dest:destination pointer
+ *@src:pointer to a string
+ *Return: concatinated string
  */
 char *_strcat(char *dest, char *src)
 {
-	int i, j;
+	char *cat = dest + _strlen(dest);
+	int length =  _strlen(dest) + _strlen(src);
 
-	i = 0;
-	j = 0;
-
-	while (dest[i] != '\0')
-		i++;
-
-	while (src[j] != '\0')
+	while (*src)
 	{
-		dest[i] = src[j];
-		j++;
-		i++;
+		*cat += *src;
+		src++;
+		cat++;
 	}
+	*cat += '\0';
+	cat -= (length);
+	*dest = *cat;
 
-	dest[i] = '\0';
-
-	return (dest);
+	return (cat);
 }
